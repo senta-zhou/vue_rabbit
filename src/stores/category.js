c67a2f705a812faf67941f6529e840c8caf6a370
Miles <miles@MilesdeMacBook-Air.local>
2026-04-19 16:40:24 +0800

    pinia优化导航数据

---
+++ src/stores/category.js
@@ -0,0 +1,17 @@
+import { ref } from "vue";
+import { defineStore } from "pinia";
+import { getCategoryAPI } from "@/apis/layout";
+
+export const useCategoryStore = defineStore("category", () => {
+  // 导航列表的数据管理
+  // state 导航列表的数据
+  const categoryList = ref([]);
+  // action 导航列表的方法
+  const getCategory = async () => {
+    const res = await getCategoryAPI();
+    console.log(res);
+    categoryList.value = res.result;
+  };
+
+  return { categoryList, getCategory };
+});
--- src/views/Layout/index.vue
+++ src/views/Layout/index.vue
@@ -3,6 +3,18 @@ import LayoutNav from "./components/LayoutNav.vue";
 import LayoutHeader from "./components/LayoutHeader.vue";
 import LayoutFooter from "./components/LayoutFooter.vue";
 import LayoutFixed from "./components/LayoutFixed.vue";
+
+// 触发获取导航的action
+// 第一步导入写好的仓库
+import { useCategoryStore } from "@/stores/category";
+// 导入vue页面挂载完成的钩子，onMounted
+import { onMounted } from "vue";
+// 实例化仓库，相当于拿到仓库的使用权
+const categoryStore = useCategoryStore();
+// 页面挂载后，调用仓库里的getXXX方法，接入数据
+onMounted(() => {
+  categoryStore.getCategory();
+});
 </script>
 
 <template>
