f838314d8451ff727a7da94985330b283a9abd25
Miles <miles@MilesdeMacBook-Air.local>
2026-04-21 20:54:31 +0800

    图片懒加载实现

--- src/main.js
+++ src/main.js
@@ -7,6 +7,7 @@ import router from "./router";
 // 引入初始化样式文件
 import "@/styles/common.scss";
 
+import { useIntersectionObserver } from "@vueuse/core";
 // // 测试接口函数
 // import { getCategory } from "@/apis/testAPI";
 // getCategory();
@@ -16,3 +17,18 @@ app.use(createPinia());
 app.use(router);
 
 app.mount("#app");
+
+// 定义全局指令
+app.directive("img-lazy", {
+  mounted(el, binding) {
+    // el指令绑定的那个元素
+    // binding.value 指令等于号后面的值， imgUrl
+    console.log(el, binding.value);
+    useIntersectionObserver(el, ([{ isIntersecting }]) => {
+      console.log(isIntersecting);
+      if (isIntersecting) {
+        el.src = binding.value;
+      }
+    });
+  },
+});
