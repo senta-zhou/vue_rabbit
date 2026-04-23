ed65712bbfe0164475e1c15cb83b6a172c0d38dd
Miles <miles@MilesdeMacBook-Air.local>
2026-04-23 16:32:11 +0800

    二级分类-整体认识和配置

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,7 @@ import Layout from "@/views/Layout/LayoutIndex.vue";
 import Login from "@/views/Login/index.vue";
 import Home from "@/views/Home/HomeIndex.vue";
 import Category from "@/views/Category/CategoryIndex.vue";
+import SubCategory from "@/views/SubCategory/SubCategoryIndex.vue";
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -16,6 +17,10 @@ const router = createRouter({
         {
           path: "category/:id",
           component: Category,
+        },
+        {
+          path: "category/sub/:id",
+          component: SubCategory,
         },
       ],
     },
