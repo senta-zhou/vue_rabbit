331ecffbf3d490eda83af6e19e721f7071883cf4
Miles <miles@MilesdeMacBook-Air.local>
2026-04-29 21:38:54 +0800

    表单的检验

--- src/views/Login/LoginIndex.vue
+++ src/views/Login/LoginIndex.vue
@@ -1,4 +1,21 @@
-<script setup></script>
+<script setup>
+import { ref } from "vue";
+
+// 1.准备表单对象
+const form = ref({
+  account: "",
+  password: "",
+});
+
+// 2.准备规则对象
+const rules = {
+  account: [{ required: true, message: "用户名不能为空", trigger: "blur" }],
+  password: [
+    { required: true, message: "密码不能为空", trigger: "blur" },
+    { min: 6, max: 14, message: "密码长度必须在 6-14 位之间", trigger: "blur" },
+  ],
+};
+</script>
 
 <template>
   <div>
@@ -21,12 +38,18 @@
         </nav>
         <div class="account-box">
           <div class="form">
-            <el-form label-position="right" label-width="60px" status-icon>
-              <el-form-item label="账户">
-                <el-input />
+            <el-form
+              :model="form"
+              :rules="rules"
+              label-position="right"
+              label-width="60px"
+              status-icon
+            >
+              <el-form-item prop="account" label="账户">
+                <el-input v-model="form.account" />
               </el-form-item>
-              <el-form-item label="密码">
-                <el-input />
+              <el-form-item prop="password" label="密码">
+                <el-input v-model="form.password" />
               </el-form-item>
               <el-form-item label-width="22px">
                 <el-checkbox size="large"> 我已同意隐私条款和服务条款 </el-checkbox>
