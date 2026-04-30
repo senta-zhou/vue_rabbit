c0f6ecb77e79bfd387d17eeda1b638076badaf0a
Miles <miles@MilesdeMacBook-Air.local>
2026-04-30 20:08:14 +0800

    登录-pinia数据持久化

--- src/stores/user.js
+++ src/stores/user.js
@@ -3,14 +3,20 @@ import { defineStore } from "pinia";
 import { ref } from "vue";
 import { loginAPI } from "@/apis/user";
 
-export const useUserStore = defineStore("user", () => {
-  // 定义管理用户的state
-  const userInfo = ref({});
-  // 定义获取接口数据action的函数
-  const getUserInfo = async ({ account, password }) => {
-    const res = await loginAPI({ account, password });
-    userInfo.value = res.result;
-  };
-  // 以对象的格式把state和action return
-  return { userInfo, getUserInfo };
-});
+export const useUserStore = defineStore(
+  "user",
+  () => {
+    // 定义管理用户的state
+    const userInfo = ref({});
+    // 定义获取接口数据action的函数
+    const getUserInfo = async ({ account, password }) => {
+      const res = await loginAPI({ account, password });
+      userInfo.value = res.result;
+    };
+    // 以对象的格式把state和action return
+    return { userInfo, getUserInfo };
+  },
+  {
+    persist: true,
+  },
+);
