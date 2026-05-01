07b4f9f721e7b9c19ca7a34bd40f41f76a59eab2
Miles <miles@MilesdeMacBook-Air.local>
2026-05-01 20:13:09 +0800

    本地购物车-头部购物车-计算属性

--- src/stores/cartStore.js
+++ src/stores/cartStore.js
@@ -1,5 +1,5 @@
 import { defineStore } from "pinia";
-import { ref } from "vue";
+import { computed, ref } from "vue";
 
 export const useCartStore = defineStore(
   "cart",
@@ -23,8 +23,16 @@ export const useCartStore = defineStore(
       const index = cartList.value.findIndex((item) => skuId === item.skuId);
       cartList.value.splice(index, 1);
     };
+
+    // 计算属性
+    // 总的数量
+    const allCount = computed(() => cartList.value.reduce((a, c) => a + c.count, 0));
+    // 总的价格
+    const allPrice = computed(() => cartList.value.reduce((a, c) => a + c.count * c.price, 0));
     return {
       cartList,
+      allCount,
+      allPrice,
       addCart,
       delCart,
     };
